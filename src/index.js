73f83e156e4a43fb05324aa9494df85b022d0557
kagera <52308117+kagerka@users.noreply.github.com>
2021-02-03 16:08:33 +0300

    feat: add code that converts number to text

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let readable = [];
+    let one = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let twenty = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let hundred = ['hundred'];
+    if (!number) {
+        readable.push('zero');
+    }
+    if (number >= 100 && number < 1000) {
+        readable.push(one[Math.floor(number / 100)]);
+        readable.push(hundred[0]);
+    }
+    if ((number - (Math.floor(number / 100) * 100)) < 20) {
+        readable.push(one[number % 100]);
+    } else if (number % 10 == 0) {
+        readable.push(twenty[Math.floor((number - (Math.floor(number / 100)) * 100) / 10)]);
+    } else if ((number - (Math.floor(number / 100) * 100)) > 20) {
+        readable.push(twenty[Math.floor((number - (Math.floor(number / 100)) * 100) / 10)]);
+        readable.push(one[number % 10]);
+    }
+    readable = readable.filter(String).join(' ').toString();
+    return readable;
 }
